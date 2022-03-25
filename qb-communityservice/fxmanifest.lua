723fe963c49c1cc6d7ddef29fa926f859984f6af
Sergio Garrido <97261499+iSheesho@users.noreply.github.com>
2022-03-25 15:10:42 -0300

    Updated fxmanifest format

--- qb-communityservice/fxmanifest.lua
+++ qb-communityservice/fxmanifest.lua
@@ -1,12 +1,15 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+
+game 'gta5'
 
 description 'Community service. Converted from ESX to QBCore'
 
 author 'Sheesho // Apostolos_Iatridis'
 
-version '1.0'
+version '1.1'
 
 server_scripts {
+	'@oxmysql/lib/MySQL.lua',
 	'locale.lua',
 	'locales/en.lua',
 	'config.lua',
