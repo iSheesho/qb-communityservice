3ef865a71ff61faf0826b1bf5de4cd04af1eaf45
Sergio Garrido <97261499+iSheesho@users.noreply.github.com>
2022-03-25 15:08:23 -0300

    Added /pcomserv command and oxmysql updates
    Added /pcomserv command for cops.
    
    Updated for optimal use of oxmysql

--- qb-communityservice/server/main.lua
+++ qb-communityservice/server/main.lua
@@ -23,11 +23,33 @@ QBCore.Commands.Add("endcomserv", "End Community Service", { { name = "id", help
 end, "admin")
 
 
+QBCore.Commands.Add("pcomserv", _U('give_player_community'), { { name = "id", help = _U('target_id') } }, false, function(source, args, user)
+    local Player = QBCore.Functions.GetPlayer(source)
+
+    local job = Player.PlayerData.job.name
+
+    if job == Config.PoliceJobName then
+
+	    if args[1] and GetPlayerName(args[1]) ~= nil and tonumber(args[2]) then
+
+		TriggerEvent('qb-communityservice:sendToCommunityService', tonumber(args[1]), tonumber(args[2]))
+
+		else
+
+		TriggerClientEvent('chat:addMessage', source, { args = { _U('system_msn'), _U('invalid_player_id_or_actions') } } )
+
+		end
+
+	end
+end, "user")
+
+
 -- unjail after time served
 RegisterServerEvent('qb-communityservice:finishCommunityService')
 AddEventHandler('qb-communityservice:finishCommunityService', function()
 	releaseFromCommunityService(source)
 end)
+
 
 RegisterServerEvent('qb-communityservice:endCommunityServiceCommand')
 AddEventHandler('qb-communityservice:endCommunityServiceCommand', function(source)
@@ -36,18 +58,19 @@ AddEventHandler('qb-communityservice:endCommunityServiceCommand', function(sourc
 	end
 end)
 
+
 RegisterServerEvent('qb-communityservice:completeService')
 AddEventHandler('qb-communityservice:completeService', function()
 
 	local _source = source
 	local identifier = QBCore.Functions.GetPlayer(_source).PlayerData.citizenid
 
-		exports['oxmysql']:execute('SELECT * FROM communityservice WHERE identifier = @identifier', {
+		MySQL.Async.fetchAll('SELECT * FROM communityservice WHERE identifier = @identifier', {
 		['@identifier'] = identifier
 	}, function(result)
 
 		if result[1] then
-				exports['oxmysql']:execute('UPDATE communityservice SET actions_remaining = actions_remaining - 1 WHERE identifier = @identifier', {
+				MySQL.Async.fetchAll('UPDATE communityservice SET actions_remaining = actions_remaining - 1 WHERE identifier = @identifier', {
 				['@identifier'] = identifier
 			})
 		else
@@ -57,20 +80,18 @@ AddEventHandler('qb-communityservice:completeService', function()
 end)
 
 
-
-
 RegisterServerEvent('qb-communityservice:extendService')
 AddEventHandler('qb-communityservice:extendService', function()
 
 	local _source = source
 	local identifier = QBCore.Functions.GetPlayer(_source).PlayerData.citizenid
 
-		exports['oxmysql']:execute('SELECT * FROM communityservice WHERE identifier = @identifier', {
+		MySQL.Async.fetchAll('SELECT * FROM communityservice WHERE identifier = @identifier', {
 		['@identifier'] = identifier
 	}, function(result)
 
 		if result[1] then
-				exports['oxmysql']:execute('UPDATE communityservice SET actions_remaining = actions_remaining + @extension_value WHERE identifier = @identifier', {
+				MySQL.Async.fetchAll('UPDATE communityservice SET actions_remaining = actions_remaining + @extension_value WHERE identifier = @identifier', {
 				['@identifier'] = identifier,
 				['@extension_value'] = Config.ServiceExtensionOnEscape
 			})
@@ -81,22 +102,21 @@ AddEventHandler('qb-communityservice:extendService', function()
 end)
 
 
-
 RegisterServerEvent('qb-communityservice:sendToCommunityService')
 AddEventHandler('qb-communityservice:sendToCommunityService', function(target, actions_count)
 
 	local identifier = QBCore.Functions.GetPlayer(target).PlayerData.citizenid
 
-		exports['oxmysql']:execute('SELECT * FROM communityservice WHERE identifier = @identifier', {
+		MySQL.Async.fetchAll('SELECT * FROM communityservice WHERE identifier = @identifier', {
 		['@identifier'] = identifier
 	}, function(result)
 		if result[1] then
-				exports['oxmysql']:execute('UPDATE communityservice SET actions_remaining = @actions_remaining WHERE identifier = @identifier', {
+				MySQL.Async.fetchAll('UPDATE communityservice SET actions_remaining = @actions_remaining WHERE identifier = @identifier', {
 				['@identifier'] = identifier,
 				['@actions_remaining'] = actions_count
 			})
 		else
-				exports['oxmysql']:execute('INSERT INTO communityservice (identifier, actions_remaining) VALUES (@identifier, @actions_remaining)', {
+				MySQL.Async.fetchAll('INSERT INTO communityservice (identifier, actions_remaining) VALUES (@identifier, @actions_remaining)', {
 				['@identifier'] = identifier,
 				['@actions_remaining'] = actions_count
 			})
@@ -105,15 +125,15 @@ AddEventHandler('qb-communityservice:sendToCommunityService', function(target, a
 
 	TriggerClientEvent('chat:addMessage', -1, { args = { _U('judge'), _U('comserv_msg', GetPlayerName(target), actions_count) }, color = { 147, 196, 109 } })
 	TriggerClientEvent('qb-communityservice:inCommunityService', target, actions_count)
-
 end)
+
 
 RegisterServerEvent('qb-communityservice:checkIfSentenced')
 AddEventHandler('qb-communityservice:checkIfSentenced', function()
 	local _source = source -- cannot parse source to client trigger for some weird reason
 	local identifier = QBCore.Functions.GetPlayer(_source).PlayerData.citizenid -- get steam identifier
 
-		exports['oxmysql']:execute('SELECT * FROM communityservice WHERE identifier = @identifier', {
+		MySQL.Async.fetchAll('SELECT * FROM communityservice WHERE identifier = @identifier', {
 		['@identifier'] = identifier
 	}, function(result)
 		if result[1] ~= nil and result[1].actions_remaining > 0 then
@@ -122,14 +142,15 @@ AddEventHandler('qb-communityservice:checkIfSentenced', function()
 	end)
 end)
 
+
 function releaseFromCommunityService(target)
 
 	local identifier = QBCore.Functions.GetPlayer(target).PlayerData.citizenid
-		exports['oxmysql']:execute('SELECT * FROM communityservice WHERE identifier = @identifier', {
+		MySQL.Async.fetchAll('SELECT * FROM communityservice WHERE identifier = @identifier', {
 		['@identifier'] = identifier
 	}, function(result)
 		if result[1] then
-				exports['oxmysql']:execute('DELETE from communityservice WHERE identifier = @identifier', {
+				MySQL.Async.fetchAll('DELETE from communityservice WHERE identifier = @identifier', {
 				['@identifier'] = identifier
 			})
 
